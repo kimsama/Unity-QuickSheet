d06cdd8b2a1efb3d20e03f911ec77cb0b8379ed4
kimsama <hwpeterkim@gmail.com>
2015-01-03 13:56:41 +0900

    - Changed 'SpreadSheetPro' in paths to 'QuickSheet'. - Modified to correctly set default template path when a asset file for excel or google setting is created.

--- ExcelPlugin/Editor/ExcelMachine.cs
+++ ExcelPlugin/Editor/ExcelMachine.cs
@@ -10,13 +10,33 @@ using System.Collections;
 
 namespace UnityEditor
 {
+    /// <summary>
+    /// A class for various setting to read excel file and generated related script files.
+    /// </summary>
     internal class ExcelMachine : BaseMachine
     {
+        /// <summary>
+        /// where the .xsl or .xslx file is. The path should start with "Assets/". 
+        /// </summary>
         public string excelFilePath;
 
         // both are needed for popup editor control.
         public string[] SheetNames = { "" };
         public int CurrentSheetIndex { get; set; }
+
+        // excel and google plugin have its own template files, 
+        // so we need to set the different path when the asset file is created.
+        private readonly string excelTemplatePath = "QuickSheet/ExcelPlugin/Templates";
+
+        /// <summary>
+        /// Note: Called when the asset file is selected.
+        /// </summary>
+        void OnEnable()
+        {
+            base.OnEnable();
+
+            TemplatePath = excelTemplatePath;
+        }
 
         /// <summary>
         /// A menu item which create a 'ExcelMachine' asset file.
