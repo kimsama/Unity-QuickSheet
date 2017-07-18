542387aff440c659f0cf750cd555bc8e15749d55
kimsama <hwpeterkim@gmail.com>
2017-07-18 22:10:25 +0900

    Modified to be able to save 'Template Path' on the 'Excel Setting' and 'Google Setting' .asset file

--- Assets/QuickSheet/ExcelPlugin/Editor/ExcelSettingsEditor.cs
+++ Assets/QuickSheet/ExcelPlugin/Editor/ExcelSettingsEditor.cs
@@ -17,11 +17,9 @@ namespace UnityQuickSheet
     [CustomEditor(typeof(ExcelSettings))]
     public class ExcelSettingsEditor : Editor
     {
-        ExcelSettings setting;
-
         public void OnEnable()
         {
-            setting = target as ExcelSettings;
+
         }
 
         public override void OnInspectorGUI()
@@ -32,47 +30,26 @@ namespace UnityQuickSheet
 
             EditorGUILayout.Separator();
 
-            // paths for runtime and editor folder which will contain generated script files.
             GUILayout.BeginHorizontal();
-            GUILayout.Label("Setting FilePath: ", GUILayout.Width(110));
-            // prevent to modify by manual
-            GUILayout.TextField(ExcelSettings.AssetPath, 120);
-            GUILayout.EndHorizontal();
-
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Setting FileName: ", GUILayout.Width(110));
-            // read-only
-            GUILayout.TextField(ExcelSettings.AssetFileName, 120);
+            GUILayout.Label("Template Path: ", GUILayout.Width(100));
+            ExcelSettings.Instance.TemplatePath = GUILayout.TextField(ExcelSettings.Instance.TemplatePath);
             GUILayout.EndHorizontal();
 
             EditorGUILayout.Separator();
 
-            if (setting.CheckPath())
-            {
-                GUILayout.BeginHorizontal();
-                GUILayout.Label("Runtime Path: ", GUILayout.Width(100));
-                setting.RuntimePath = GUILayout.TextField(setting.RuntimePath);
-                GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Runtime Path: ", GUILayout.Width(100));
+            ExcelSettings.Instance.RuntimePath = GUILayout.TextField(ExcelSettings.Instance.RuntimePath);
+            GUILayout.EndHorizontal();
 
-                GUILayout.BeginHorizontal();
-                GUILayout.Label("Editor Path: ", GUILayout.Width(100));
-                setting.EditorPath = GUILayout.TextField(setting.EditorPath);
-                GUILayout.EndHorizontal();
-            }
-            else
-            {
-                GUILayout.BeginHorizontal();
-                GUILayout.Toggle(true, "", "CN EntryError", GUILayout.Width(20));
-                GUILayout.BeginVertical();
-                GUILayout.Label("", GUILayout.Height(12));
-                GUILayout.Label("Correct the path of the ExcelSetting.asset file.", GUILayout.Height(20));
-                GUILayout.EndVertical();
-                GUILayout.EndHorizontal();
-            }
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Editor Path: ", GUILayout.Width(100));
+            ExcelSettings.Instance.EditorPath = GUILayout.TextField(ExcelSettings.Instance.EditorPath);
+            GUILayout.EndHorizontal();
 
             if (GUI.changed)
             {
-                EditorUtility.SetDirty(setting);
+                EditorUtility.SetDirty(ExcelSettings.Instance);
             }
         }
     }
