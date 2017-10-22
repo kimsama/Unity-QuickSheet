caf5e3cabbe2404374a0be9ec718ddd4fb802dd1
Johannes Deml <send@johannesdeml.com>
2017-10-22 19:35:52 +0200

    Add example sheet

---
+++ Assets/Editor/ExcelExampleEditor.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnityQuickSheet;
+
+///
+/// !!! Machine generated code !!!
+///
+[CustomEditor(typeof(ExcelExample))]
+public class ExcelExampleEditor : BaseExcelEditor<ExcelExample>
+{	    
+    public override bool Load()
+    {
+        ExcelExample targetData = target as ExcelExample;
+
+        string path = targetData.SheetName;
+        if (!File.Exists(path))
+            return false;
+
+        string sheet = targetData.WorksheetName;
+
+        ExcelQuery query = new ExcelQuery(path, sheet);
+        if (query != null && query.IsValid())
+        {
+            targetData.dataArray = query.Deserialize<ExcelExampleData>().ToArray();
+            EditorUtility.SetDirty(targetData);
+            AssetDatabase.SaveAssets();
+            return true;
+        }
+        else
+            return false;
+    }
+}
