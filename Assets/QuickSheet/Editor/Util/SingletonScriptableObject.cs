2abde67945bd3dd64c3ddefc689aa8df36eee2b7
Zhixiang Zhu <zzxiang21cn@hotmail.com>
2019-02-12 14:54:09 +0800

    Automatically find settings file

--- Assets/QuickSheet/Editor/Util/SingletonScriptableObject.cs
+++ Assets/QuickSheet/Editor/Util/SingletonScriptableObject.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace UnityQuickSheet
@@ -22,7 +23,28 @@ namespace UnityQuickSheet
             get
             {
                 if (!_instance)
+                {
                     _instance = Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+                    if (!_instance)
+                    {
+                        string filter = "t:" + typeof(T);
+                        var guids = AssetDatabase.FindAssets(filter);
+                        if (guids.Length > 0)
+                        {
+                            if (guids.Length > 1)
+                                Debug.LogWarningFormat("Multiple {0} assets are found.", typeof(T));
+
+                            string assetPath = AssetDatabase.GUIDToAssetPath(guids[0]);
+                            Debug.LogFormat("Using {0}.", assetPath);
+                            _instance = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+                        }
+                        // no found any setting file.
+                        else
+                        {
+                            Debug.LogWarning("No instance of " + typeof(T).Name + " is loaded. Please create a " + typeof(T).Name + " asset file.");
+                        }
+                    }
+                }
                 return _instance;
             }
         }
